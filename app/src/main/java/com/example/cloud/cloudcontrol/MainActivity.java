6dcfc3c03be1ad10b2b6f0e6280bdf6152665e21
jakub <qbabor4444@gmail.com>
2017-06-13 18:05:45 +0200

    more

--- app/src/main/java/com/example/cloud/cloudcontrol/MainActivity.java
+++ app/src/main/java/com/example/cloud/cloudcontrol/MainActivity.java
@@ -2,19 +2,35 @@ package com.example.cloud.cloudcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        changeText();
+        buttonClick();
     }
 
-    private void changeText(){
-        //TextView tv = (TextView)findViewById(R.id.textView);
-        //tv.setText("hello");
+    private void changeTextViewText(){ // jaki id i na jaki text
+        TextView tv = (TextView)findViewById(R.id.textView6);
+        num++;
+        tv.setText(String.valueOf(num));
+    }
+
+    private void buttonClick(){
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        changeTextViewText();
+                    }
+                }
+        );
     }
 }
