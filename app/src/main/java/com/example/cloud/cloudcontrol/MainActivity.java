be6f82f03d1b6c1db97990c99c8d07b0d4e36593
jakub <qbabor4444@gmail.com>
2017-06-13 17:11:21 +0200

    first commit

---
+++ app/src/main/java/com/example/cloud/cloudcontrol/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.cloud.cloudcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        changeText();
+    }
+
+    private void changeText(){
+        //TextView tv = (TextView)findViewById(R.id.textView);
+        //tv.setText("hello");
+    }
+}
