7cc8255a68816ef0dc83e27e6cb731b60b5c104b
jakub <qbabor4444@gmail.com>
2017-06-21 21:24:44 +0200

    lol

---
+++ app/src/main/java/com/example/cloud/cloudcontrol/StratupActivity.java
@@ -0,0 +1,19 @@
+package com.example.cloud.cloudcontrol;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StratupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.mainAppTheme);
+        super.onCreate(savedInstanceState);
+        //Intent bluetoothConnectionIntent = new Intent(this, BluetoothConnection.class);
+        //startActivity( bluetoothConnectionIntent  );
+
+        //finish();
+        //setContentView(R.layout.activity_stratup);
+    }
+}
